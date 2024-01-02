5e0d31fadef579a421657a9492fbd54d693bfbfa
Murat Koyun <muratk1845@gmail.com>
2024-01-02 18:27:10 +0100

    rework

--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 
+//syntactical sugar///
 const fetchData = async () => {
     try {
         const res = await fetch('http//regres.in/api/users')
@@ -13,3 +14,19 @@ const fetchData = async () => {
         console.log(error)
     }
 }
+// traditional .then method//
+const dataFetcher = () => {
+    fetch('http//regres.in/api/users')
+        .then(res => {
+            if(!res.ok) {  //guard clause//
+                console.log("somethign went wrong")
+                return;
+            }
+            res.json()
+        }).then(data => {
+            console.log(data)
+        }).catch(error => {
+            console.log(error)
+        })
+}
+    
